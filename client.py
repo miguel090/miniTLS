589fc357feef1a3c5f5c9371cae8253f4c137fa4
miguel090 <miguelaqcs@gmail.com>
2020-02-29 02:45:06 +0000

    changed AES to CTR mode and sends json files now

--- client.py
+++ client.py
@@ -1,8 +1,9 @@
 
 # Import socket module
 import socket
+import json
+from base64 import b64encode, b64decode
 from Crypto.Cipher import AES
-from Crypto.Util.Padding import pad, unpad
 from Crypto.Random import get_random_bytes
 
 def Main():
@@ -17,31 +18,35 @@ def Main():
     # connect to server on local computer
     s.connect((host,port))
     data = s.recv(1024)
-    print(data.decode('ascii'))
+    print(data.decode('utf-8'))
     #key = get_random_bytes(32)
-    #iv = get_random_bytes(16)
+
     # message you send to server
     while True:
 
         message=input()
-        message = message.encode('ascii')
         if message=='' : continue
-        #create AES object
-        AESenc = AES.new(b'This is a key123', AES.MODE_CBC, b'This is an IV456')
-        ciphertext = AESenc.encrypt(pad(message,16))
-        print(ciphertext)
+        message = message.encode('utf-8')
+        #create AES object and encrypt
+        AESenc = AES.new(b'This is a key123', AES.MODE_CTR)
+        ciphertext_bytes = AESenc.encrypt(message)
+        ciphertext = b64encode(ciphertext_bytes).decode('utf-8')
+        nonce = b64encode(AESenc.nonce).decode('utf-8')
+        #create json file
+        result = json.dumps({'nonce':nonce, 'ciphertext':ciphertext})
         # message sent to server
-        s.send(ciphertext)
+        s.send(result.encode('utf-8'))
 
-        data = s.recv(1024)
-        AESdec = AES.new(b'This is a key123', AES.MODE_CBC, b'This is an IV456')
-        message = unpad(AESdec.decrypt(data), 16)
         # messaga received from server
-
+        data = s.recv(1024)
+        rcv_json = json.loads(data)
+        nonce= b64decode(rcv_json['nonce'])
+        ciphertext= b64decode(rcv_json['ciphertext'])
+        AESdec = AES.new(b'This is a key123', AES.MODE_CTR, nonce=nonce)
+        message = AESdec.decrypt(ciphertext)
 
         # print the received message
-        # here it would be a reverse of sent message
-        print('Received from the server :',str(message.decode('ascii')))
+        print('Received from the server :',str(message.decode('utf-8')))
 
         if message != 'quit':
             continue
