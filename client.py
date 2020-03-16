64c6a62431dc40e2ba47c3c9049c0a6e438adefd
FredyR4zox <fredyr4zox@localhost.localdomain>
2020-03-16 12:21:28 +0000

    Made class3

--- client.py
+++ client.py
@@ -7,96 +7,132 @@ from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 from Crypto.Hash import HMAC, SHA256
 
+charset = 'utf-8'
+
+
 def encData(message, enc_key):
-    #create AES object and encrypt
+    # create AES object and encrypt
     AESenc = AES.new(enc_key, AES.MODE_CTR)
     ciphertext = AESenc.encrypt(message)
 
     return ciphertext, AESenc.nonce
 
+
 def decData(ciphertext, nonce, enc_key):
     AESdec = AES.new(enc_key, AES.MODE_CTR, nonce=nonce)
     return AESdec.decrypt(ciphertext)
+
 
 def make_auth(ciphertext, auth_key):
     h = HMAC.new(auth_key, digestmod=SHA256)
     h.update(ciphertext)
     return h.digest()
 
+
 def verify_auth(mac, msg, auth_key):
     h = HMAC.new(auth_key, digestmod=SHA256)
     h.update(msg)
     try:
-        #need to transform from hexadecimal bytes to hexadecimal string
+        # need to transform from hexadecimal bytes to hexadecimal string
         h.hexverify(mac.hex())
         print('mac is good')
     except ValueError as e:
         print('major error')
     return
 
+
 def make_json(ciphertext_bytes, nonce, mac):
-    #b64 encode becuz of weird characters
-    ciphertext = b64encode(ciphertext_bytes).decode('utf-8')
-    nonce = b64encode(nonce).decode('utf-8')
-    mac = b64encode(mac).decode('utf-8')
-    return json.dumps({'nonce':nonce, 'ciphertext':ciphertext, 'mac':mac})
+    # b64 encode becuz of weird characters
+    ciphertext = b64encode(ciphertext_bytes).decode(charset)
+    nonce = b64encode(nonce).decode(charset)
+    mac = b64encode(mac).decode(charset)
+
+    return json.dumps({'nonce': nonce, 'ciphertext': ciphertext, 'mac': mac})
+
 
 def parse_json(data_rcv):
     rcv_json = json.loads(data_rcv)
-    ciphertext= b64decode(rcv_json['ciphertext'])
-    nonce= b64decode(rcv_json['nonce'])
-    mac = b64decode(rcv_json['mac'])
-    return ciphertext, nonce, mac
-    
-def Main():
-    # local host IP '127.0.0.1'
-    host = '127.0.0.1'
 
+    ciphertext = b64decode(rcv_json['ciphertext'])
+    nonce = b64decode(rcv_json['nonce'])
+    mac = b64decode(rcv_json['mac'])
+
+    return ciphertext, nonce, mac
+
+
+def Main():
+    # Local host IP '127.0.0.1'
+    host = '127.0.0.1'
     # Define the port on which you want to connect
     port = 9999
 
-    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    # connect to server on local computer
-    s.connect((host,port))
-    data = s.recv(1024)
-    print(data.decode('utf-8'))
-    enc_key = b'aaaaatadinhadela'#get_random_bytes(32)
-    auth_key= b'tadinhadelaaaaaa'#get_random_bytes(32)
-    # message you send to server
+    # Connect to server on local computer
+    s.connect((host, port))
+    sessionKey = s.recv(32)
+    print('Received the following session key: ' + str(sessionKey) + '\n')
+
+    enc_key = SHA256.new(data=sessionKey + b'1').digest()
+    auth_key = SHA256.new(data=sessionKey + b'2').digest()
+
+    enc = AES.new(enc_key, AES.MODE_CTR, nonce=b'1234')
+    dec = AES.new(enc_key, AES.MODE_CTR, nonce=b'1234')
+    ehmac = HMAC.new(auth_key, digestmod=SHA256)
+    dhmac = HMAC.new(auth_key, digestmod=SHA256)
+
+    message = s.recv(2048)
+    print('Received the following message: ' + str(message) + '\n')
+
     while True:
-
-        message=input()
-        if message=='' : continue
-        message = message.encode('utf-8')
-        #encrypt data with AES-CTR mode
-        ciphertext, nonce = encData(message, enc_key)
-        mac = make_auth(message, auth_key)
-        #create json file
-        result = make_json(ciphertext, nonce, mac)
-        # message sent to server
-        s.send(result.encode('utf-8'))
-
-        # message received from server
-        data_rcv = s.recv(1024)
         try:
-            ciphertext ,nonce ,mac= parse_json(data_rcv)
-            #decode the json received
-            message = decData(ciphertext, nonce, enc_key)
-        except ValueError | KeyError as e:
-            print('error in decrtytion')
-
-        #validate mac
-        verify_auth(mac, message, auth_key)
-        # print the received message
-        print('Received from the server :',str(message.decode('utf-8')))
-
-        if message != 'quit':
+            message = input('Message to send: ')
+        except EOFError as e:
+            print("Type 'quit' or 'exit' to exit the program gracefully.")
             continue
-        else:
+
+        if message == '':
+            print("Type 'quit' or 'exit' to exit the program gracefully.")
+            continue
+        elif message == 'quit' or message == 'exit':
             break
-    # close the connection
+
+        message = message.encode(charset)
+        ciphertext = enc.encrypt(message)
+
+        mac = ehmac.update(message).digest()
+
+        # Create json file
+        result = make_json(ciphertext, enc.nonce, mac)
+
+        # Message sent to server
+        s.send(result.encode(charset))
+
+        # Message received from server
+        data_rcv = s.recv(2048)
+        try:
+            ciphertext, nonce, mac = parse_json(data_rcv)
+            # Decode the json received
+            message = dec.decrypt(ciphertext)
+        except ValueError | KeyError as e:
+            print('Error in decryption')
+
+        # Validate mac
+        dhmac.update(message)
+        try:
+            # need to transform from hexadecimal bytes to hexadecimal string
+            dhmac.hexverify(mac.hex())
+            print('MAC is good')
+        except ValueError as e:
+            print('Error in MAC')
+
+        # Print the received message
+        print('Received from the server: ' +
+              str(message.decode(charset)) + '\n')
+
+    # Close the connection
     s.close()
+
 
 if __name__ == '__main__':
     Main()
--- server.py
+++ server.py
@@ -1,18 +1,42 @@
 #!/usr/bin/env python
 
-import socket, threading
+import socket
+import threading
+import json
+from base64 import b64encode, b64decode
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+from Crypto.Hash import HMAC, SHA256
+
 
 class ClientThread(threading.Thread):
 
-    def __init__(self,ip,port,clientsocket):
+    def __init__(self, ip, port, clientsocket):
         threading.Thread.__init__(self)
         self.ip = ip
         self.port = port
         self.csocket = clientsocket
-        print ( "[+] New thread started for "+ip+":"+str(port))
+        self.charset = 'utf-8'
+        self.nonce = b'1234'
+
+        self.sessionKey = get_random_bytes(32)
+        self.enc_key = SHA256.new(data=self.sessionKey + b'1').digest()
+        self.auth_key = SHA256.new(data=self.sessionKey + b'2').digest()
+
+        self.enc = AES.new(self.enc_key, AES.MODE_CTR, nonce=self.nonce)
+        self.dec = AES.new(self.enc_key, AES.MODE_CTR, nonce=self.nonce)
+        self.ehmac = HMAC.new(self.auth_key, digestmod=SHA256)
+        self.dhmac = HMAC.new(self.auth_key, digestmod=SHA256)
+
+        print("[+] New thread started for " + ip + ":" + str(port))
 
     def run(self):
-        print ( "Connection from : "+ip+":"+str(port))
+        print("Connection from " + self.ip + ":" + str(self.port))
+
+        print("Session key for " + self.ip + ":" +
+              str(self.port) + ": " + str(self.sessionKey))
+        self.csocket.send(self.sessionKey)
+
         stri = "Welcome to the server"
         send = stri.encode('ascii')
         self.csocket.send(send)
@@ -21,14 +45,50 @@ class ClientThread(threading.Thread):
 
         while len(data):
             data = self.csocket.recv(2048)
-            print(data)
-            #encode to ascii data received
-            #dataSTR = data.decode('ascii')
-            #print(dataSTR)
-            #print ("Client(", self.ip," :", str(self.port),") sent : ", dataSTR)
+            print("Received the following data: " + str(data))
+            if data == b'':
+                break
+            try:
+                ciphertext, nonce, mac = self.parse_json(data)
+                # Decode the json received
+                message = self.dec.decrypt(ciphertext)
+            except ValueError | KeyError as e:
+                print('Error in decryption')
+
+            # Validate mac
+            self.dhmac.update(message)
+            try:
+                # need to transform from hexadecimal bytes to hexadecimal string
+                self.dhmac.hexverify(mac.hex())
+                print('MAC is good')
+            except ValueError as e:
+                print('Error in MAC')
+
+            # Print the received message
+            print('Received from the client: ' +
+                  str(message.decode(self.charset)) + '\n')
+
             self.csocket.send(data)
 
-        print ( "Client at "+self.ip+" disconnected...")
+        print("Client at " + self.ip + " disconnected...")
+
+    def make_json(self, ciphertext_bytes, nonce, mac):
+        # b64 encode becuz of weird characters
+        ciphertext = b64encode(ciphertext_bytes).decode(self.charset)
+        nonce = b64encode(nonce).decode(self.charset)
+        mac = b64encode(mac).decode(self.charset)
+
+        return json.dumps({'nonce': nonce, 'ciphertext': ciphertext, 'mac': mac})
+
+    def parse_json(self, data_rcv):
+        rcv_json = json.loads(data_rcv)
+
+        ciphertext = b64decode(rcv_json['ciphertext'])
+        nonce = b64decode(rcv_json['nonce'])
+        mac = b64decode(rcv_json['mac'])
+
+        return ciphertext, nonce, mac
+
 
 host = "127.0.0.1"
 port = 9999
@@ -36,13 +96,13 @@ port = 9999
 tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 tcpsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-tcpsock.bind((host,port))
+tcpsock.bind((host, port))
+print("Listening for incoming connections...\n")
 
 while True:
     tcpsock.listen(4)
-    print ("Listening for incoming connections...")
     (clientsock, (ip, port)) = tcpsock.accept()
 
-    #pass clientsock to the ClientThread thread object being created
+    # pass clientsock to the ClientThread thread object being created
     newthread = ClientThread(ip, port, clientsock)
     newthread.start()
