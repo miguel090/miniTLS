fa4ef65cb8504ea991a2ed47bd24712cb4e2f5de
miguel090 <miguelaqcs@gmail.com>
2020-04-26 15:54:09 +0100

    station to station protocol implemented

--- client.py
+++ client.py
@@ -1,4 +1,3 @@
-
 # Import socket module
 import socket
 import json
@@ -6,34 +5,43 @@ from base64 import b64encode, b64decode
 from Crypto.Cipher import AES, ARC4
 from Crypto.Random.random import randrange
 from Crypto.Hash import HMAC, SHA256
+from Crypto.PublicKey import ECC
+from Crypto.Signature import DSS
 
 from asn1crypto.keys import DSAParams
 from os import system
 
 charset = 'utf-8'
+
+
 class Key:
     def __init__(self):
-        #code got in slack by student with name up201005324
-        system("openssl dsaparam -outform DER -in parameters.pem -out parameters.der" )
-        with open("parameters.der", "rb") as f:
+        # code got in slack by student with name up201005324
+        system("openssl dsaparam -outform DER -in parameters2.pem -out parameters2.der")
+        with open("parameters2.der", "rb") as f:
             certs = f.read()
         params = DSAParams.load(certs)
-        self.p=int(params['p'])
-        self.q= int(params['q'])
-        self.g=int(params['g'])
+        self.p = int(params['p'])
+        self.q = int(params['q'])
+        self.g = int(params['g'])
+        f.close()
+
 
 class Client:
     def __init__(self):
         self.nonce = b'1234'
+
+        key = Key()
+        self.client_secret = randrange(0, key.q - 1)
+        self.shared_prime = key.p
+        self.shared_base = key.g
+        self.client_DHside = pow(self.shared_base, self.client_secret, self.shared_prime)
+
+        # everything is set after establishing session key
         self.sessionKey = 0
         self.enc_key = 0
         self.auth_key = 0
-
-        key = Key()
-        self.client_secret = randrange(0,key.q-1)
-        self.shared_prime = key.p
-        self.shared_base=key.g
-        self.client_public_key= pow(self.shared_base, self.client_secret, self.shared_prime)
+        self.clientnr = 0
 
         self.encCipher = 0
         self.decCipher = 0
@@ -42,19 +50,71 @@ class Client:
 
         self.nrseq = 0
 
-def estabilish_session_key(s, client):
-    #print("client_public_key: " + str(client.client_public_key))
-    s.send(str(client.client_public_key).encode(charset))
 
-    server_public_key = s.recv(4096)
-    #print("server_public_key: " + str(server_public_key))
-    server_public_key = int(server_public_key.decode(charset))
+def establish_session_key(s, client):
+    # print("client_DHside: " + str(client.client_DHside))
+    s.send(str(client.client_DHside).encode(charset))
 
-    client.sessionKey = pow(server_public_key, client.client_secret, client.shared_prime)
-    #print(client.sessionKey)
-    client.sessionKey= str(client.sessionKey).encode(charset)
-
+    server_DHside_signature_clientnr = s.recv(4096)
+    # print("server_DHside: " + str(server_DHside))
+    server_DHside, encrypted_json = get_server_DHside_and_encrypted_json(server_DHside_signature_clientnr)
+    sessionKey = pow(server_DHside, client.client_secret, client.shared_prime)
+    client.sessionKey = str(sessionKey).encode(charset)
     set_keys(client)
+
+    signed_hash_json = dec_data(client, encrypted_json).decode(charset)
+    client.clientnr, signed_hash = get_clientnr_and_hash_from_json(signed_hash_json)
+    #print(signed_hash)
+    hashed_values, validity = verify_hash(client, signed_hash, server_DHside)
+    if validity == False:
+        return False
+    create_asymetric_key_files(client.clientnr)
+
+    signed_hash = sign_DHvalues(client, hashed_values)
+    message_to_send = enc_data(client, signed_hash)
+    message_to_send = b64encode(message_to_send)
+    s.send(message_to_send)
+    # print(client.sessionKey)
+    return True
+
+
+def sign_DHvalues(client, hashed_values):
+    f = open(str(client.clientnr) + '_private_key.pem')
+    key = ECC.import_key(f.read())
+
+    signer = DSS.new(key, 'fips-186-3')
+    signed_hash = signer.sign(hashed_values)
+
+    return signed_hash
+
+
+def verify_hash(client, signed_hash, server_DHside):
+    verify_values = json.dumps({'gy': str(server_DHside), 'gx': str(client.client_DHside)})
+    hash_to_verify = SHA256.new(verify_values.encode(charset))
+
+    #print('hashed values: ' + hash_to_verify.hexdigest())
+    #print('signed value: ' + str(signed_hash))
+    f = open('server_public_key.pem')
+    key = ECC.import_key(f.read())
+
+    verifier = DSS.new(key, 'fips-186-3')
+    try:
+        verifier.verify(hash_to_verify, signed_hash)
+        #print("verified signature")
+        return hash_to_verify, True
+    except ValueError:
+        print("The message signature couldnt be validated")
+        return hash_to_verify, False
+
+
+def get_clientnr_and_hash_from_json(signed_hash_json):
+    rcv_json = json.loads(signed_hash_json)
+    return rcv_json['clientnr'], b64decode(rcv_json['signature'])
+
+
+def get_server_DHside_and_encrypted_json(server_DHside_signature_clientnr):
+    rcv_json = json.loads(server_DHside_signature_clientnr)
+    return int(rcv_json['gy']), b64decode(rcv_json['encrypted_signed'])
 
 
 def set_keys(client):
@@ -66,29 +126,29 @@ def set_keys(client):
     client.ehmac = HMAC.new(client.auth_key, digestmod=SHA256)
     client.dhmac = HMAC.new(client.auth_key, digestmod=SHA256)
 
-def encData(client, message):
+
+def enc_data(client, message):
     return client.encCipher.encrypt(message)
 
 
-def decData(client, ciphertext):
+def dec_data(client, ciphertext):
     return client.decCipher.decrypt(ciphertext)
 
-
 def make_auth(client, ciphertext):
-    #add sequence number to ciphertext
+    # add sequence number to ciphertext
     ciphertext = ciphertext + bytes(client.nrseq)
     client.ehmac.update(ciphertext)
     return client.ehmac.digest()
 
 
 def verify_auth(client, ciphertext, mac):
-    #add sequence number to ciphertext
+    # add sequence number to ciphertext
     ciphertext = ciphertext + bytes(client.nrseq)
     client.dhmac.update(ciphertext)
     try:
         # need to transform from hexadecimal bytes to hexadecimal string
         client.dhmac.hexverify(mac.hex())
-        print('MAC is good.')
+        #print('MAC is good.')
     except ValueError as e:
         print('MAC with error.')
         return 1
@@ -127,11 +187,16 @@ def Main():
     s.connect((host, port))
 
     client = Client()
-    estabilish_session_key(s, client)
+    if establish_session_key(s, client) == False:
+        print("There was an error estabilishing the key")
+        s.send("Couldnt establish shared key".encode(charset))
+        return
+
     #print('Received the following session key: ' + str(client.sessionKey) + '\n')
     message = s.recv(2048)
     print('Received the following greeting message: ' + str(message) + '\n')
-
+    #reset keys so values coincide again
+    set_keys(client)
     while True:
         try:
             message = input('Message to send: ')
@@ -146,7 +211,7 @@ def Main():
             break
 
         message = message.encode(charset)
-        ciphertext = encData(client, message)
+        ciphertext = enc_data(client, message)
 
         mac = make_auth(client, message)
 
@@ -161,10 +226,10 @@ def Main():
         try:
             ciphertext, client.nonce, mac = parse_json(data_rcv)
             # Decode the json received
-            message = decData(client, ciphertext)
+            message = dec_data(client, ciphertext)
+            #print(message)
         except ValueError | KeyError as e:
             print('Error in decryption')
-
         # Validate mac
         if verify_auth(client, message, mac) != 0:
             print("Security error. Closing connection")
@@ -177,6 +242,18 @@ def Main():
 
     # Close the connection
     s.close()
+
+
+def create_asymetric_key_files(client_nr):
+    key = ECC.generate(curve='P-256')
+
+    f = open(str(client_nr) + '_private_key.pem', 'wt')
+    f.write(key.export_key(format='PEM'))
+    f.close()
+
+    f = open(str(client_nr) + '_public_key.pem', 'wt')
+    f.write(key.export_key(format='PEM'))
+    f.close()
 
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -7,38 +7,49 @@ from base64 import b64encode, b64decode
 from Crypto.Cipher import AES
 from Crypto.Random.random import randrange
 from Crypto.Hash import HMAC, SHA256
+from Crypto.PublicKey import ECC
+from Crypto.Signature import DSS
 
 from asn1crypto.keys import DSAParams
 from os import system
 
+charset = 'utf-8'
+
+
 class Key:
     def __init__(self):
-        #code got in slack by student with name up201005324
-        system("openssl dsaparam -outform DER -in parameters.pem -out parameters.der" )
-        with open("parameters.der", "rb") as f:
+        # code got in slack by student with name up201005324
+        system("openssl dsaparam -outform DER -in parameters1.pem -out parameters1.der")
+        with open("parameters1.der", "rb") as f:
             certs = f.read()
         params = DSAParams.load(certs)
-        self.p=int(params['p'])
-        self.q= int(params['q'])
-        self.g=int(params['g'])
+        self.p = int(params['p'])
+        self.q = int(params['q'])
+        self.g = int(params['g'])
+        f.close()
+
 
 class ClientThread(threading.Thread):
 
-    def __init__(self, ip, port, clientsocket):
+    def __init__(self, ip, port, client_nr, clientsocket):
         threading.Thread.__init__(self)
         self.ip = ip
         self.port = port
+        self.client_nr = client_nr
+        client_nr += 1
+
         self.csocket = clientsocket
         self.charset = 'utf-8'
         self.nonce = b'1234'
         self.nrseq = 0
 
-        key=Key()
-        self.server_secret = randrange(0,key.q-1)
+        key = Key()
+        self.server_secret = randrange(0, key.q - 1)
         self.shared_prime = key.p
-        self.shared_base= key.g
-        self.server_public_key= pow(self.shared_base, self.server_secret, self.shared_prime)
+        self.shared_base = key.g
+        self.server_DHside = pow(self.shared_base, self.server_secret, self.shared_prime)
 
+        # everything is set after establishing session key
         self.sessionKey = 0
         self.enc_key = 0
         self.auth_key = 0
@@ -50,19 +61,69 @@ class ClientThread(threading.Thread):
 
         print("[+] New thread started for " + ip + ":" + str(port))
 
-    def estabilish_session_key(self):
-        #print('server_public_key: ' + str(self.server_public_key))
-        client_public_key=self.csocket.recv(4096)
-        #print('client_public_key: ' + str(client_public_key))
-        client_public_key=int(client_public_key.decode('utf-8'))
-
-        self.csocket.send(str(self.server_public_key).encode('utf-8'))
-        self.sessionKey= pow(client_public_key, self.server_secret, self.shared_prime)
-        #print(self.sessionKey)
-        self.sessionKey=str(self.sessionKey).encode('utf-8')
+    def establish_session_key(self):
+        # print('server_public_key: ' + str(self.server_public_key))
+        client_DHside = self.csocket.recv(4096)
+        # print('client_public_key: ' + str(client_public_key))
+        client_DHside = int(client_DHside.decode(self.charset))
+        sessionKey = pow(client_DHside, self.server_secret, self.shared_prime)
+        self.sessionKey = str(sessionKey).encode(self.charset)
         self.set_keys()
 
+        # encrypted_signed_json is a json with the client nr and the signature of the hash of gy and gx
+        # it is encrypted with session key and b64 encoded
+        encrypted_signed_json, hashed_values = self.sign_client_and_DHsides(client_DHside)
+
+        data_to_send = json.dumps({'gy': str(self.server_DHside), 'encrypted_signed': encrypted_signed_json})
+
+        self.csocket.send(data_to_send.encode('utf-8'))
+
+        confirmation_data_encrypted = self.csocket.recv(4096)
+        confirmation_data = self.get_confirmation_data(confirmation_data_encrypted)
+        #The signature contains the hashed values so it verifies if the values are correct
+        if self.verify_signature(confirmation_data, hashed_values) == False:
+            return False
+        return True
+
+
+    def get_confirmation_data(self, confirmation_data_encrypted):
+        ciphertext = b64decode(confirmation_data_encrypted)
+        # returns in bytes format
+        return self.dec_data(ciphertext)
+
+    def sign_client_and_DHsides(self, client_DHside):
+        # make json with both values to send
+        signing_items = json.dumps({'gy': str(self.server_DHside), 'gx': str(client_DHside)})
+        hash_to_sign = SHA256.new(signing_items.encode(self.charset))
+
+        f = open('server_private_key.pem')
+        key = ECC.import_key(f.read())
+
+        signer = DSS.new(key, 'fips-186-3')
+        signature = signer.sign(hash_to_sign)
+        #print('hashed_values: ' + hash_to_sign.hexdigest())
+        #print('signed value: ' + str(signature))
+        signature = b64encode(signature).decode(self.charset)
+        signed_json = json.dumps({'signature': signature, 'clientnr': str(self.client_nr)})
+
+        encrypted_signed_json = self.enc_data(signed_json.encode(self.charset))
+
+        encrypted_signed_json = b64encode(encrypted_signed_json).decode(charset)
+        return encrypted_signed_json, hash_to_sign
+
+    def verify_signature(self, data_signed, data_to_test):
+        key = ECC.import_key(open(str(self.client_nr) + '_public_key.pem').read())
+        verifier = DSS.new(key, 'fips-186-3')
+        try:
+            test = verifier.verify(data_to_test, data_signed)
+            print("verified signature")
+            return True
+        except ValueError:
+            print("The message signature couldnt be validated")
+            return False
+
     def set_keys(self):
+
         self.enc_key = SHA256.new(data=self.sessionKey + b'1').digest()
         self.auth_key = SHA256.new(data=self.sessionKey + b'2').digest()
 
@@ -71,21 +132,20 @@ class ClientThread(threading.Thread):
         self.ehmac = HMAC.new(self.auth_key, digestmod=SHA256)
         self.dhmac = HMAC.new(self.auth_key, digestmod=SHA256)
 
-    def encData(self, message):
+    def enc_data(self, message):
+        # message must be bytes
         return self.enc.encrypt(message)
 
-
-    def decData(self, ciphertext):
+    def dec_data(self, ciphertext):
         return self.dec.decrypt(ciphertext)
 
-
     def make_auth(self, mac, ciphertext):
+        ciphertext = ciphertext + bytes(self.nrseq)
         self.ehmac.update(ciphertext)
         return self.ehmac.digest()
 
-
     def verify_auth(self, ciphertext, mac):
-        #add sequence number to ciphertext
+        # add sequence number to ciphertext
         ciphertext = ciphertext + bytes(self.nrseq)
         self.dhmac.update(ciphertext)
         try:
@@ -117,10 +177,13 @@ class ClientThread(threading.Thread):
 
     def run(self):
         print("Connection from " + self.ip + ":" + str(self.port))
-        self.estabilish_session_key()
-        print("Session key for " + self.ip + ":" +
-              str(self.port) + ": " + str(self.sessionKey))
+        if self.establish_session_key() == False:
+            print("There was an error estabilishing the key")
+            self.csocket.send("Couldnt establish shared key").encode(self.charset)
+            return
 
+        #print("Session key for " + self.ip + ":" + str(self.port) + ": " + str(self.sessionKey))
+        self.set_keys()
         stri = "Welcome to the server"
         send = stri.encode('ascii')
         self.csocket.send(send)
@@ -135,9 +198,11 @@ class ClientThread(threading.Thread):
                 break
 
             try:
+
                 ciphertext, nonce, mac = self.parse_json(data)
                 # Decode the json received
-                message = self.decData(ciphertext)
+                message = self.dec_data(ciphertext)
+
             except ValueError | KeyError as e:
                 print('Error in decryption')
 
@@ -147,7 +212,7 @@ class ClientThread(threading.Thread):
                 break
 
             # Print the received message
-            print('Received from the client: ' +
+            print('Received from the client ' + str(self.client_nr) + ': ' +
                   str(message.decode(self.charset)) + '\n')
 
             self.csocket.send(data)
@@ -156,19 +221,39 @@ class ClientThread(threading.Thread):
         print("Client at " + self.ip + " disconnected...")
 
 
-host = "127.0.0.1"
-port = 9999
+def create_asymetric_key_files():
+    key = ECC.generate(curve='P-256')
 
-tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-tcpsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    f = open('server_private_key.pem', 'wt')
+    f.write(key.export_key(format='PEM'))
+    f.close()
 
-tcpsock.bind((host, port))
-print("Listening for incoming connections...\n")
+    f = open('server_public_key.pem', 'wt')
+    f.write(key.export_key(format='PEM'))
+    f.close()
 
-while True:
-    tcpsock.listen(4)
-    (clientsock, (ip, port)) = tcpsock.accept()
 
-    # pass clientsock to the ClientThread thread object being created
-    newthread = ClientThread(ip, port, clientsock)
-    newthread.start()
+def Main():
+    create_asymetric_key_files()
+
+    host = "127.0.0.1"
+    port = 9999
+
+    tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcpsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    tcpsock.bind((host, port))
+    print("Listening for incoming connections...\n")
+    client_nr = 0
+    while True:
+        tcpsock.listen(4)
+        (clientsock, (ip, port)) = tcpsock.accept()
+
+        # pass clientsock to the ClientThread thread object being created
+        newthread = ClientThread(ip, port, client_nr, clientsock)
+        newthread.start()
+        client_nr += 1
+
+
+if __name__ == '__main__':
+    Main()
